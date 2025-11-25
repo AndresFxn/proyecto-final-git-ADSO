dac88688f9890039177874b3c0ab41117c43d466
Andrés Felipe <andresforerol8512@gmail.com>
2025-11-24 23:37:46 -0500

    Agregar estructura base de la rama feature/login

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 console.log("Proyecto Final Git – ADSO");
+
+
+/*
+  Rama: feature/login
+  Fecha: 2025
+  Descripción: Lógica inicial del sistema de login.
+*/
+
+function login(usuario, contraseña) {
+  if (usuario === "admin" && contraseña === "1234") {
+    return "Acceso concedido";
+  } else {
+    return "Credenciales incorrectas";
+  }
+}
+
+console.log(login("admin", "1234"));
